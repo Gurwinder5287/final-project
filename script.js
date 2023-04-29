4be1c7617bc79dcec18331d527731eb537e71d9b
Gurwinder5287 <124536176+Gurwinder5287@users.noreply.github.com>
2023-04-29 00:03:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar. addEventListener('click', () => {
+        nav.classList.add('active');
+
+    })
+}
+
+if (close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+
+    })
+}
+
+$(document).ready(function(){
+    update_amounts();
+    $('.qty, .price').on('keyup keypress blur change',
+    function(e){
+        update_amounts();
+    });
+});
+function update_amounts(){
+    var sum = 0.0;
+    $('#myTable > tbody > tr').each(function(){
+        var qty = $(this).find('.qty').val();
+        var price = $(this).find('.price').val();
+        var amount = (qty*price)
+        sum+=amount;
+        $(this).find('.amount').text(''+amount);
+    });
+    $('.total').text(sum);
+}
+
+var incrementQty;
+var decrementQty;
+var plusBtn = $(".cart-qty-plus");
+var minusBtn = $(".cart-qty-minus");
+var incrementQty = plusBtn.click(function(){
+    var $n = $(this)
+    .parent(".button-container")
+    .find(".qty");
+    $n.val(Number($n.val())+1);
+    update_amounts();
+});
+
+var decrementQty = minusBtn.click(function(){
+    var $n = $(this)
+    .parent(".button-container")
+    .find(".qty");
+    var QtyVal = Number($n.val());
+    if (QtyVal > 0) {
+        $n.val(QtyVal-1);
+    }
+    update_amounts();
+});
+
